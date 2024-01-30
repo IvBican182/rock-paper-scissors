73b0d2dd7b4f9dcdde04a14219fb21c6dace5472
IvBican182 <ibicanic@live.com>
2024-01-30 21:00:37 +0100

    adding comments and changes in headers

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("hi");
 
-const options =["rock","paper","scissors"];
+const options =["rock","paper","scissors"]; //adding our options as strings in an array
 
 const playScoreSpan = document.querySelector(".play-Score")
 const compScoreSpan = document.querySelector(".comp-Score")
@@ -8,6 +8,7 @@ const runScore = document.querySelector(".run-score");
 const p1 = document.createElement("p1");
 runScore.appendChild(p1);
 
+//adding buttons for player choices
 const rockButton = document.querySelector(".rock")
 const paperButton = document.querySelector(".paper")
 const scissorsButton = document.querySelector(".scissors")
@@ -15,15 +16,17 @@ const scissorsButton = document.querySelector(".scissors")
 const outcomeDiv = document.querySelector(".outcome")
 const restartDiv = document.querySelector(".restart")
 
-
+//returns a random computer choice
 function getComputerChoice() {
     let choice = options[Math.floor(Math.random() * options.length)];
     return choice;
     console.log(choice);
 }
 
+
 getComputerChoice();
 
+//game logic for winning or losing the game
 function checkWinner(playerSelection,computerSelection) {
     if (playerSelection == computerSelection) {
         return "Tie";
@@ -40,19 +43,19 @@ function checkWinner(playerSelection,computerSelection) {
     else { return "Computer";
     }
 }
-
+//creating variables to track the score
 let playerScore = 0;
 let computerScore = 0;
 let gameScore = 0;
 
-
+//adding results for rounds, adding results to html
 function playRound(playerSelection,computerSelection) {
     const result = checkWinner(playerSelection,computerSelection);
     if (result == "Tie") {
         const p = document.createElement("p");
         p.innerText = "It's a tie";
         outcomeDiv.appendChild(p);
-        gameScore++
+        gameScore++ //appending the game score
         
 
     }
@@ -75,12 +78,13 @@ function playRound(playerSelection,computerSelection) {
 
 
 }
-
+//rendering scores on screen
 function updateScore(playerScore,computerScore) {
     p1.innerText = `Player Score: ${playerScore}  Computer Score: ${computerScore}`;
 
 }
 
+//no longer in use, user was asked to type the choice, now it is on-click
 function getPlayerChoice() {
     let validatedInput = false;
     while(validatedInput == false) {
@@ -96,7 +100,7 @@ function getPlayerChoice() {
     }
 
 }
-
+//changes the player selection variable to a clicked button
 rockButton.addEventListener("click", () => {
     const playerSelection = "rock";
     const computerSelection = getComputerChoice();
@@ -149,9 +153,9 @@ function handler(e) {
 function checkForWinner (playerScore, computerScore) {
     if (playerScore > computerScore && gameScore == 5) {
             const h1 = document.createElement("h1");
-            h1.innerText = `You win the game by: ${playerScore} to ${computerScore}`;
+            h1.innerText = `You win the game by: ${playerScore} to ${computerScore}`; //winning game output
             outcomeDiv.append(h1);
-            const restartBtn = document.createElement("button")
+            const restartBtn = document.createElement("button") //reseting the window onclick
             restartBtn.addEventListener("click", () => {
                 window.location.reload();
             })
@@ -164,15 +168,15 @@ function checkForWinner (playerScore, computerScore) {
             
             
     }
-    else if (playerScore < computerScore && gameScore == 5){
+    else if (playerScore < computerScore && gameScore == 5){ //tracks the game till 5 rounds are completed
             const h1 = document.createElement("h1");
-            h1.innerText = `You lose the game by: ${computerScore} to ${playerScore}`;
+            h1.innerText = `You lose the game by: ${computerScore} to ${playerScore}`; //losing game output
             outcomeDiv.append(h1);
             const restartBtn = document.createElement("button")
             restartBtn.addEventListener("click", () => {
                 window.location.reload();
             })
-            restartBtn.setAttribute('style', 'color: blue; background: purple; padding: 20px; width: 200px; text-align: center;');
+            restartBtn.setAttribute('style', 'color: blue; background: purple; padding: 20px; width: 200px; text-align: center;'); //adding a restard game button
             restartBtn.textContent = 'RESTART';
             restartDiv.appendChild(restartBtn);
             document.addEventListener("click", handler, true);
@@ -181,7 +185,7 @@ function checkForWinner (playerScore, computerScore) {
     }
     else if (playerScore == computerScore && gameScore == 5) {
             const h1 = document.createElement("h1");
-            h1.innerText = `It's a tie game: ${computerScore} , ${playerScore}`;
+            h1.innerText = `It's a tie game: ${computerScore} , ${playerScore}`; //tie game output
             outcomeDiv.append(h1);
             const restartBtn = document.createElement("button")
             restartBtn.addEventListener("click", () => {
@@ -201,33 +205,3 @@ function checkForWinner (playerScore, computerScore) {
 
     
 
-
-
-/*
-function game() {
-    let scorePlayer = 0;
-    let scoreComputer = 0;
-    for(let i=0; i<5;i++) {
-        const playerSelection = getPlayerChoice();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection,computerSelection));
-        console.log("--------------------");
-        if(checkWinner(playerSelection,computerSelection) == "Player") {
-            scorePlayer++;
-        }
-        else if(checkWinner(playerSelection,computerSelection) == "Computer") {
-            scoreComputer++;
-        }
-    }
-    console.log("game over");
-    if(scorePlayer > scoreComputer) {
-        console.log("You win");
-    }else if (scoreComputer > scorePlayer) {
-        console.log("you lose")
-    }else {
-        console.log("it's a tie");
-    }
-}
-
-game();
-*/
